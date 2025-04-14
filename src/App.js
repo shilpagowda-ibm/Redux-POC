2d2afcbadd0a00ff12100f48b65ffbccca93f4ad
Shilpa gowda <shilpa.shilpa@ibm.com>
2025-04-14 17:40:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './features/counter/counterSlice';
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Redux Counter</h1>
+      <h2>{count}</h2>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
